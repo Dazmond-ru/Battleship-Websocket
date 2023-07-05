167b68a4f98efcadf892efcb7d204dca7e45ed99
Sergey <fserega2014@yandex.ru>
2023-07-05 10:33:30 +0300

    feat: migrate project to TS

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import { httpServer } from "./src/http_server/index.js";
+import dotenv from 'dotenv'
 
-const HTTP_PORT = 8181;
+dotenv.config()
+const HTTP_PORT = process.env.PORT || 8181
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
